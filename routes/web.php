4ef52ec748f06a9372241bf6eef932ad2de7a53a
Michael <michael@MichaeldeMacBook-Pro.local>
2022-08-02 20:31:54 +0800

    add inertia js and breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/video_demo', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/video_demo_2', function () {
-    return view('welcome2');
-});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+require __DIR__.'/auth.php';
